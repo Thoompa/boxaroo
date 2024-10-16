e15e96463fbc345f33458c7242582fca4a1e3b0e
TimHarris96 <59544597+TimHarris96@users.noreply.github.com>
2024-10-16 20:24:51 +1100

    Header and initial stylings

---
+++ BoxarooApp/src/components/AppHeader.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+defineProps<{
+  msg: string
+}>()
+</script>
+
+<template>
+  <div class="header">Boxaroo</div>
+</template>
+
+<style scoped>
+.header {
+  border-bottom: 1px solid white;
+  font-size: xx-large;
+  display: flex;
+  justify-content: center;
+}
+</style>
